c86321a8660b05f93692c7bd2dd8f0fbb0f6e0f0
Tusharsolanki077 <tusharsolanki077@gmail.com>
2023-02-06 22:24:37 +0530

    added productCard

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,18 +1,33 @@
-import React, {useEffect} from 'react'
+import React, { Fragment , useEffect } from 'react'
 import './Home.css'
+import ProductCard from '../layout/product/productCard';
 
 import { getProduct } from "../../actions/productAction";
-import {useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const Home = () => {
 
     const dispatch = useDispatch();
+    const {error,products} = useSelector((state) => state.products);
 
     useEffect(() => {
+        if (error) {
+          
+          dispatch();
+        }
         dispatch(getProduct());
-    }, [dispatch]);
+      }, [dispatch, error]);
+
+
 
     return (
+
+       
+       
+
+        <Fragment>
+
         <div className='home'>
             <div className="home__container">
                 <img
@@ -20,12 +35,27 @@ const Home = () => {
                     src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                     alt=""
                 />
+
+                <h2 className="homeHeading">Featured Products</h2>
+                <div className='products'>
+
+                    {products && 
+                        products.map((product) => (
+                            <ProductCard key={product._id} product={product} />
+                        ))
+                    }
+                </div>
             </div>
 
-            
+        </div>
+        
+        </Fragment>
 
-            </div>
-            )
+
+        
+
+        
+    )
 }
 
-            export default Home
+export default Home
---
+++ frontend/src/components/layout/product/productCard.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './productCard.css'
+
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component"
+
+import img from "../../../assets/earphone.jpg"
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const productCard = (product) => {
+
+    const options = {
+        value: product.ratings,
+        readOnly: true,
+        precision: 0.5,
+      };
+
+    return (
+
+        <Link className="product" to={`/product/${product._id}`}>
+            <div className="image">
+                <img src={img} alt={product.name} />
+            </div>
+            <div className="namePrice">
+                <h3>{product.name}</h3>
+            </div>
+            <p>{product.description}</p>
+            <div>
+                <ReactStars {...options}/><span>({product.numOfReviews} Reviews)</span>
+            </div>
+
+            <button className="btn">
+                <span className="price">{`₹${product.price}`}</span>
+                <span className="shopping-cart"><i className="fa fa-shopping-cart" aria-hidden="true"><ShoppingCartIcon /></i></span>
+                <span className="buy">Buy Now</span>
+            </button>
+        </Link>
+    )
+}
+
+export default productCard
